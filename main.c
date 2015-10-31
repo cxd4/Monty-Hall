211e7fad299432aa3ae2022e1d23774a2959f7dd
unknown <rj150@hotmail.com>
2015-10-31 19:05:36 -0400

    rewrote game show round loop to offer automated testing

--- main.c
+++ main.c
@@ -5,31 +5,47 @@
 size_t wins  = 0;
 size_t total = 0;
 
-static void round_execute(void);
+static void round_execute(unsigned int door_number, int switching);
 
-int main(void)
+int main(int argc, char* argv[])
 {
-    for (;;)
-        round_execute();
+    if (argc < 2)
+        for (;;)
+            round_execute(NUMBER_OF_DOORS, -1);
     return 0;
 }
 
-static void round_execute(void)
+static void round_execute(unsigned int door_number, int switching)
 {
+    unsigned int hint_number;
+
  /* Randomly arrange what's behind all three doors. */
     set_doors();
 
-    puts("Choose a door:");
-    show_doors();
-    printf("Pick from 1 to %d (0 for random):  ", NUMBER_OF_DOORS);
-    select_door(get_door_number());
+    while (door_number >= NUMBER_OF_DOORS) {
+        puts("Choose a door:");
+        show_doors();
+        printf("Pick from 1 to %d:  ", NUMBER_OF_DOORS);
+        door_number = get_door_number();
+    }
+    select_door(door_number);
 
+    hint_number = hint_door();
     printf(
         "Before I reveal what's behind door %u, here's door %u.\n",
-        current_guess() + 1, hint_door() + 1
+        door_number + 1, hint_number + 1
     );
-    fputs("Which door is your final guess?  ", stdout);
-    select_door(get_door_number());
+    if (switching < 0) { /* Ask whether to switch. */
+        fputs("Which door is your final guess?  ", stdout);
+        door_number = get_door_number();
+    } else if (switching == 0) {
+     /* Skip asking for final answer; just go to revealing the prize. */
+    } else { /* Skip asking; automatically switch to a remaining door. */
+        do {
+            door_number = (door_number + 1) % NUMBER_OF_DOORS;
+        } while (door_number == hint_number);
+    }
+    select_door(door_number);
 
     reveal_all_doors();
     if (current_guess() == winning_door()) {
